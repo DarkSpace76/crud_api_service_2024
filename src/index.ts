fbb2f56f283b0a8a3856ee3005c97fe923094944
DarkSpace76 <84933249+DarkSpace76@users.noreply.github.com>
2024-10-22 00:16:25 +0300

    feat: add user object

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,43 @@ export let masterPort;
 export const args = parseArgs();
 export const app = http.createServer(!args['cluster'] ? route : routeCluster);
 
+
+if (!args['cluster']) {
+
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}/`);
+    });
+
+} else {
+
+    if (cluster.isPrimary) {
+
+
+        for (let index = 0; index < cpus; index++) {
+            const worker = cluster.fork({ PORT: PORT + index + 1, MASTERPORT: PORT });
+
+            workers.push({ worker: worker, isActive: false, handler: null });
+        }
+
+        app.listen(PORT, () => {
+            console.log(
+                `Primary ${process.pid} is running at http://localhost:${PORT}/`,
+            );
+        });
+
+
+
+    } else {
+        const processPort = process.env.PORT;
+        masterPort = process.env.MASTERPORT;
+
+        app.listen(processPort, () => {
+
+            console.log(
+                `Worker ${process.pid} server running at http://localhost:${processPort}/`,
+            );
+        });
+    }
+
+}
+
